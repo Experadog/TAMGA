053bb9e148dc817864c614c576f46ee18bd0ab59
Urmat Kokonbaev <kokonbaevurmat4@gmail.com>
2025-03-19 23:23:04 +0600

    added i18m

---
+++ src/app/[locale]/error.jsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { useTranslations } from 'next-intl';
+import { useEffect } from 'react';
+
+
+export default function Error({ error, reset }) {
+    const t = useTranslations('Error');
+
+    useEffect(() => {
+        console.error(error);
+    }, [error]);
+
+    return (
+        <div>
+            {t.rich('description', {
+                p: (chunks) => <p className="mt-4">{chunks}</p>,
+                retry: (chunks) => (
+                    <button
+                        className="text-white underline underline-offset-2"
+                        onClick={reset}
+                        type="button"
+                    >
+                        {chunks}
+                    </button>
+                )
+            })}
+        </div>
+    );
+}
